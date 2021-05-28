1000412d5a4dd3ebea50ec08506cfde9fdfb6a41
dsevostianov <dsevostianov@neoflex.ru>
2021-05-28 11:20:58 +0300

    final

---
+++ Form1.Designer.cs
@@ -0,0 +1,414 @@
+﻿
+namespace HAP
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.otchBox = new System.Windows.Forms.TextBox();
+            this.surBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ageBox = new System.Windows.Forms.TextBox();
+            this.genBox = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.bilBox = new System.Windows.Forms.TextBox();
+            this.label18 = new System.Windows.Forms.Label();
+            this.creatineBox = new System.Windows.Forms.TextBox();
+            this.lpBox = new System.Windows.Forms.TextBox();
+            this.label26 = new System.Windows.Forms.Label();
+            this.label27 = new System.Windows.Forms.Label();
+            this.kdrlgBox = new System.Windows.Forms.TextBox();
+            this.textBox20 = new System.Windows.Forms.TextBox();
+            this.label31 = new System.Windows.Forms.Label();
+            this.fvBox = new System.Windows.Forms.TextBox();
+            this.ttgBox = new System.Windows.Forms.TextBox();
+            this.ishemiaBox = new System.Windows.Forms.ComboBox();
+            this.label33 = new System.Windows.Forms.Label();
+            this.depresBox = new System.Windows.Forms.ComboBox();
+            this.label36 = new System.Windows.Forms.Label();
+            this.label37 = new System.Windows.Forms.Label();
+            this.aldesteronBox = new System.Windows.Forms.TextBox();
+            this.label47 = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(132, 27);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(121, 23);
+            this.nameBox.TabIndex = 0;
+            // 
+            // otchBox
+            // 
+            this.otchBox.Location = new System.Drawing.Point(656, 27);
+            this.otchBox.Name = "otchBox";
+            this.otchBox.Size = new System.Drawing.Size(121, 23);
+            this.otchBox.TabIndex = 1;
+            // 
+            // surBox
+            // 
+            this.surBox.Location = new System.Drawing.Point(402, 27);
+            this.surBox.Name = "surBox";
+            this.surBox.Size = new System.Drawing.Size(121, 23);
+            this.surBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(13, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Имя:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(269, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Фамилия:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(529, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Отчество:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(789, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Возраст:";
+            // 
+            // ageBox
+            // 
+            this.ageBox.Location = new System.Drawing.Point(922, 27);
+            this.ageBox.Name = "ageBox";
+            this.ageBox.Size = new System.Drawing.Size(121, 23);
+            this.ageBox.TabIndex = 7;
+            // 
+            // genBox
+            // 
+            this.genBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.genBox.FormattingEnabled = true;
+            this.genBox.Items.AddRange(new object[] {
+            "Мужской",
+            "Женский"});
+            this.genBox.Location = new System.Drawing.Point(1169, 27);
+            this.genBox.Name = "genBox";
+            this.genBox.Size = new System.Drawing.Size(121, 23);
+            this.genBox.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(1049, 35);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(33, 15);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Пол:";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.ForeColor = System.Drawing.Color.Black;
+            this.label15.Location = new System.Drawing.Point(13, 80);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(72, 15);
+            this.label15.TabIndex = 31;
+            this.label15.Text = "Билирубин:";
+            // 
+            // bilBox
+            // 
+            this.bilBox.Location = new System.Drawing.Point(132, 77);
+            this.bilBox.Name = "bilBox";
+            this.bilBox.Size = new System.Drawing.Size(121, 23);
+            this.bilBox.TabIndex = 30;
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.ForeColor = System.Drawing.Color.Black;
+            this.label18.Location = new System.Drawing.Point(279, 77);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(55, 15);
+            this.label18.TabIndex = 37;
+            this.label18.Text = "Креатин:";
+            // 
+            // creatineBox
+            // 
+            this.creatineBox.Location = new System.Drawing.Point(402, 77);
+            this.creatineBox.Name = "creatineBox";
+            this.creatineBox.Size = new System.Drawing.Size(121, 23);
+            this.creatineBox.TabIndex = 36;
+            // 
+            // lpBox
+            // 
+            this.lpBox.Location = new System.Drawing.Point(922, 69);
+            this.lpBox.Name = "lpBox";
+            this.lpBox.Size = new System.Drawing.Size(121, 23);
+            this.lpBox.TabIndex = 42;
+            // 
+            // label26
+            // 
+            this.label26.AutoSize = true;
+            this.label26.ForeColor = System.Drawing.Color.Black;
+            this.label26.Location = new System.Drawing.Point(794, 77);
+            this.label26.Name = "label26";
+            this.label26.Size = new System.Drawing.Size(53, 15);
+            this.label26.TabIndex = 53;
+            this.label26.Text = "ЛП (см):";
+            this.label26.Click += new System.EventHandler(this.label26_Click);
+            // 
+            // label27
+            // 
+            this.label27.AutoSize = true;
+            this.label27.ForeColor = System.Drawing.Color.Black;
+            this.label27.Location = new System.Drawing.Point(794, 135);
+            this.label27.Name = "label27";
+            this.label27.Size = new System.Drawing.Size(51, 15);
+            this.label27.TabIndex = 55;
+            this.label27.Text = "КДРЛЖ:";
+            // 
+            // kdrlgBox
+            // 
+            this.kdrlgBox.Location = new System.Drawing.Point(922, 132);
+            this.kdrlgBox.Name = "kdrlgBox";
+            this.kdrlgBox.Size = new System.Drawing.Size(121, 23);
+            this.kdrlgBox.TabIndex = 54;
+            // 
+            // textBox20
+            // 
+            this.textBox20.Location = new System.Drawing.Point(1519, 190);
+            this.textBox20.Name = "textBox20";
+            this.textBox20.Size = new System.Drawing.Size(100, 23);
+            this.textBox20.TabIndex = 56;
+            // 
+            // label31
+            // 
+            this.label31.AutoSize = true;
+            this.label31.ForeColor = System.Drawing.Color.Black;
+            this.label31.Location = new System.Drawing.Point(529, 77);
+            this.label31.Name = "label31";
+            this.label31.Size = new System.Drawing.Size(26, 15);
+            this.label31.TabIndex = 63;
+            this.label31.Text = "ФВ:";
+            // 
+            // fvBox
+            // 
+            this.fvBox.Location = new System.Drawing.Point(656, 77);
+            this.fvBox.Name = "fvBox";
+            this.fvBox.Size = new System.Drawing.Size(121, 23);
+            this.fvBox.TabIndex = 62;
+            // 
+            // ttgBox
+            // 
+            this.ttgBox.Location = new System.Drawing.Point(402, 132);
+            this.ttgBox.Name = "ttgBox";
+            this.ttgBox.Size = new System.Drawing.Size(121, 23);
+            this.ttgBox.TabIndex = 64;
+            // 
+            // ishemiaBox
+            // 
+            this.ishemiaBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ishemiaBox.FormattingEnabled = true;
+            this.ishemiaBox.Items.AddRange(new object[] {
+            "Нет",
+            "Есть"});
+            this.ishemiaBox.Location = new System.Drawing.Point(1169, 69);
+            this.ishemiaBox.Name = "ishemiaBox";
+            this.ishemiaBox.Size = new System.Drawing.Size(121, 23);
+            this.ishemiaBox.TabIndex = 67;
+            // 
+            // label33
+            // 
+            this.label33.AutoSize = true;
+            this.label33.ForeColor = System.Drawing.Color.Black;
+            this.label33.Location = new System.Drawing.Point(1049, 72);
+            this.label33.Name = "label33";
+            this.label33.Size = new System.Drawing.Size(58, 15);
+            this.label33.TabIndex = 66;
+            this.label33.Text = "Ишемия:";
+            // 
+            // depresBox
+            // 
+            this.depresBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.depresBox.FormattingEnabled = true;
+            this.depresBox.Items.AddRange(new object[] {
+            "Нет",
+            "Есть"});
+            this.depresBox.Location = new System.Drawing.Point(132, 132);
+            this.depresBox.Name = "depresBox";
+            this.depresBox.Size = new System.Drawing.Size(121, 23);
+            this.depresBox.TabIndex = 73;
+            this.depresBox.SelectedIndexChanged += new System.EventHandler(this.depresBox_SelectedIndexChanged);
+            // 
+            // label36
+            // 
+            this.label36.AutoSize = true;
+            this.label36.ForeColor = System.Drawing.Color.Black;
+            this.label36.Location = new System.Drawing.Point(12, 135);
+            this.label36.Name = "label36";
+            this.label36.Size = new System.Drawing.Size(69, 15);
+            this.label36.TabIndex = 72;
+            this.label36.Text = "Депрессия:";
+            // 
+            // label37
+            // 
+            this.label37.AutoSize = true;
+            this.label37.ForeColor = System.Drawing.Color.Black;
+            this.label37.Location = new System.Drawing.Point(269, 135);
+            this.label37.Name = "label37";
+            this.label37.Size = new System.Drawing.Size(87, 15);
+            this.label37.TabIndex = 74;
+            this.label37.Text = "ТТГ, мкМЕ/мл:";
+            // 
+            // aldesteronBox
+            // 
+            this.aldesteronBox.Location = new System.Drawing.Point(656, 132);
+            this.aldesteronBox.Name = "aldesteronBox";
+            this.aldesteronBox.Size = new System.Drawing.Size(121, 23);
+            this.aldesteronBox.TabIndex = 101;
+            this.aldesteronBox.TextChanged += new System.EventHandler(this.aldesteronBox_TextChanged);
+            // 
+            // label47
+            // 
+            this.label47.AutoSize = true;
+            this.label47.ForeColor = System.Drawing.Color.Black;
+            this.label47.Location = new System.Drawing.Point(529, 135);
+            this.label47.Name = "label47";
+            this.label47.Size = new System.Drawing.Size(121, 15);
+            this.label47.TabIndex = 100;
+            this.label47.Text = "Альдостерон, пг/мл:";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(1170, 132);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(120, 23);
+            this.saveButton.TabIndex = 107;
+            this.saveButton.Text = "Расчитать";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1321, 208);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.aldesteronBox);
+            this.Controls.Add(this.label47);
+            this.Controls.Add(this.label37);
+            this.Controls.Add(this.depresBox);
+            this.Controls.Add(this.label36);
+            this.Controls.Add(this.ishemiaBox);
+            this.Controls.Add(this.label33);
+            this.Controls.Add(this.ttgBox);
+            this.Controls.Add(this.label31);
+            this.Controls.Add(this.fvBox);
+            this.Controls.Add(this.textBox20);
+            this.Controls.Add(this.label27);
+            this.Controls.Add(this.kdrlgBox);
+            this.Controls.Add(this.label26);
+            this.Controls.Add(this.lpBox);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.creatineBox);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.bilBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.genBox);
+            this.Controls.Add(this.ageBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.surBox);
+            this.Controls.Add(this.otchBox);
+            this.Controls.Add(this.nameBox);
+            this.Name = "MainForm";
+            this.Text = "Расчет вероятности инфаркта";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TextBox otchBox;
+        private System.Windows.Forms.TextBox surBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ageBox;
+        private System.Windows.Forms.ComboBox genBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.TextBox bilBox;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.TextBox creatineBox;
+        private System.Windows.Forms.TextBox lpBox;
+        private System.Windows.Forms.Label label26;
+        private System.Windows.Forms.Label label27;
+        private System.Windows.Forms.TextBox kdrlgBox;
+        private System.Windows.Forms.TextBox textBox20;
+        private System.Windows.Forms.Label label31;
+        private System.Windows.Forms.TextBox fvBox;
+        private System.Windows.Forms.TextBox ttgBox;
+        private System.Windows.Forms.ComboBox ishemiaBox;
+        private System.Windows.Forms.Label label33;
+        private System.Windows.Forms.ComboBox depresBox;
+        private System.Windows.Forms.Label label36;
+        private System.Windows.Forms.Label label37;
+        private System.Windows.Forms.TextBox aldesteronBox;
+        private System.Windows.Forms.Label label47;
+        private System.Windows.Forms.Button saveButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HAP
+{
+    public partial class MainForm : Form
+    {
+        //Фориула находится в методе HAP, все остальное готово
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (
+            nameBox.Text == "" ||
+            otchBox.Text == "" ||
+            surBox.Text == "" ||
+            ageBox.Text == "" ||
+            genBox.SelectedIndex == -1 ||
+            bilBox.Text == "" ||
+            creatineBox.Text == "" ||
+            lpBox.Text == "" ||
+            kdrlgBox.Text == "" ||
+            fvBox.Text == "" ||
+            ttgBox.Text == "" ||
+            ishemiaBox.SelectedIndex == -1 ||
+            depresBox.SelectedIndex == -1 ||
+            aldesteronBox.Text == ""
+            )
+            {
+                MessageBox.Show("Пустые обязательные поля!");
+            }
+            else
+            {
+
+                if (
+           checkDouble(ageBox.Text, "Возраст") ||       
+           checkDouble(bilBox.Text, "Билирубин") ||          
+           checkDouble(creatineBox.Text, "Креатин") ||
+           checkDouble(lpBox.Text, "ЛП") ||
+           checkDouble(kdrlgBox.Text, "КДРЛЖ") ||        
+           checkDouble(fvBox.Text, "ФВ") ||
+           checkDouble(ttgBox.Text, "ТТГ") ||         
+           checkDouble(aldesteronBox.Text, "Альдостерон"))
+                {
+                    Сalculation(depresBox.SelectedIndex, Double.Parse(creatineBox.Text), Double.Parse(kdrlgBox.Text),
+                        ishemiaBox.SelectedIndex, Double.Parse(lpBox.Text), Double.Parse(fvBox.Text),
+                        Double.Parse(aldesteronBox.Text), Double.Parse(ttgBox.Text), Double.Parse(bilBox.Text));
+                }
+            }
+
+        }
+
+        public bool checkDouble(string str, string message)
+        {
+            double num;
+            if (!Double.TryParse(str, out num))
+            {
+                MessageBox.Show("В поле " + message + " некорректное значение");
+                return false;
+            }
+            else
+                return true;
+        }
+
+
+        public void Сalculation(int depresion, double creatine, double kdrlg, int ishemia, double lp, double fv,
+            double aldesteron, double ttg, double bilirubin)
+        {
+            //Депрессия
+            double X1o; //Основная
+            double X1k; //Контрольная
+            //Креатин
+            double X2o;
+            double X2k;
+            //КДР ЛЖ
+            double X3o;
+            double X3k;
+            //Ишемия
+            double X4o;
+            double X4k;
+            //Размер ЛП
+            double X5o;
+            double X5k;
+            //ФВ ЛЖ
+            double X6o;
+            double X6k;
+            //Альдестерон
+            double X7o;
+            double X7k;
+            //ТТГ
+            double X8o;
+            double X8k;
+            //Билирубин
+            double X9o;
+            double X9k;
+
+            if (depresion == 1)
+            {
+                X1o = 0.003;
+                X1k = 0.071;
+            }
+            else
+            {
+                X1o = 0.051;
+                X1k = 0.928;
+            }
+
+            if(creatine <= 0.11)
+            {
+                X2o = 0.888;
+                X2k = 0.928;
+            }
+            else if (creatine <= 0.139)
+            {
+                X2o = 0.055;
+                X2k = 0.042;
+            }
+            else
+            {
+                X2o = 0.055;
+                X2k = 0.028;
+            }
+
+            if(kdrlg <= 4.9)
+            {
+                X3o = 0.444;
+                X3k = 0.714;
+            }
+            else if(kdrlg <= 5.9)
+            {
+                X3o = 0.388;
+                X3k = 0.257;
+            }
+            else
+            {
+                X3o = 0.166;
+                X3k = 0.028;
+            }
+
+            if(ishemia == 1)
+            {
+                X4o = 0.944;
+                X4k = 0.828;
+            }
+            else
+            {
+                X4o = 0.055;
+                X4k = 0.171;
+            }
+
+            if(lp <= 3.7)
+            {
+                X5o = 0.277;
+                X5k = 0.571;
+            }
+            else if(lp <= 4.1)
+            {
+                X5o = 0.277;
+                X5k = 0.171;
+            }
+            else
+            {
+                X5o = 0.444;
+                X5k = 0.257;
+            }    
+
+
+            if(fv <= 44)
+            {
+                X6o = 0.277;
+                X6k = 0.1;
+            }
+            else if (fv  <= 54)
+            {
+                X6o = 0.222;
+                X6k = 0.242;
+            }
+            else
+            {
+                X6o = 0.5;
+                X6k = 0.657;
+            }
+
+            if(aldesteron <= 100)
+            {
+                X7o = 0.184;
+                X7k = 0.266;
+            }
+            else if(aldesteron <= 200)
+            {
+                X7o = 0.276;
+                X7k = 0.133;
+            }
+            else if(aldesteron <= 300)
+            {
+                X7o = 0.171;
+                X7k = 0.333;
+            }
+            else
+            {
+                X7o = 0.236;
+                X7k = 0.266;
+            }
+
+            if(ttg <= 1.0)
+            {
+                X8o = 0.277;
+                X8k = 0.571;
+            }
+            else if(ttg <= 4)
+            {
+                X8o = 0.277;
+                X8k = 0.171;
+            }
+            else
+            {
+                X8o = 0.444;
+                X8k = 0.257;
+            }
+
+
+            if(bilirubin <= 11.9)
+            {
+                X9o = 0.833;
+                X9k = 0.614;
+            }
+            else if(bilirubin <= 17)
+            {
+                X9o = 0.111;
+                X9k = 0.328;
+            }
+            else
+            {
+                X9o = 0.055;
+                X9k = 0.057;
+            }
+
+            decimal osn = Convert.ToDecimal(0.205 * X1o * X2o * X3o * X4o * X5o * X6o * X7o * X8o * X9o);
+            decimal contrl = Convert.ToDecimal(0.795*X1k * X2k * X3k * X4k * X5k * X6k * X7k * X8k * X9k);
+            decimal hap = osn / (osn+contrl);
+
+            MessageBox.Show("Вероятность инфаркта = " + hap);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label26_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aldesteronBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void depresBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
